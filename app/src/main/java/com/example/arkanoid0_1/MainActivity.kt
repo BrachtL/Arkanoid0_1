1da5e076be409dc616943f776c20cd905aac71af
Luciano Bracht <lucianoeletronico@gmail.com>
2023-03-12 03:00:01 -0300

    first commit (initial setup): hiding bars and nav buttons, paddle with motion, RectEdge class and updateBallPosition() started

---
+++ app/src/main/java/com/example/arkanoid0_1/MainActivity.kt
@@ -0,0 +1,181 @@
+package com.example.arkanoid0_1
+
+import android.graphics.Color
+import android.os.*
+import android.util.DisplayMetrics
+import androidx.appcompat.app.AppCompatActivity
+import android.view.MotionEvent
+import android.view.View
+import android.view.WindowInsets
+import android.view.WindowInsetsController
+import android.widget.ImageView
+import java.util.concurrent.Executors
+
+
+import android.view.WindowManager
+
+private var immersiveFlags: Int = 0
+
+private lateinit var ballImageView: ImageView
+private lateinit var handler: Handler
+private const val updateInterval = 16L // Update every 16 milliseconds (approx. 60 FPS)
+
+private var standardSpeedY = 8
+private var standardSpeedX = 8
+
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        //hiding two bars and nav buttons
+        // Set immersive flags to hide the navigation bar and status bar
+        immersiveFlags = (
+                View.SYSTEM_UI_FLAG_IMMERSIVE_STICKY
+                        or View.SYSTEM_UI_FLAG_HIDE_NAVIGATION
+                        or View.SYSTEM_UI_FLAG_FULLSCREEN
+                        or View.SYSTEM_UI_FLAG_LAYOUT_STABLE
+                        or View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION
+                        or View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN
+                )
+        window.decorView.systemUiVisibility = immersiveFlags
+    }
+
+    override fun onResume() {
+        super.onResume()
+
+        // Re-apply immersive flags when the activity resumes
+        window.decorView.systemUiVisibility = immersiveFlags
+    }
+
+    override fun onWindowFocusChanged(hasFocus: Boolean) {
+        super.onWindowFocusChanged(hasFocus)
+
+        // Re-apply immersive flags when the window focus changes
+        if (hasFocus) {
+            window.decorView.systemUiVisibility = immersiveFlags
+        }
+
+
+
+        setContentView(R.layout.activity_main)
+
+        /*
+        val test: ImageView = findViewById(R.id.imageView)
+        test.y = 100F
+        test.x = -300F
+
+        fun onTouch(event: MotionEvent): Boolean {
+            val x = event.x
+            val y = event.y
+
+            val test: ImageView = findViewById(R.id.imageView)
+            test.x = x
+            test.y = y
+
+            return true
+        }
+        */
+
+        val displayMetrics = DisplayMetrics()
+        windowManager.defaultDisplay.getMetrics(displayMetrics)
+        val screenHeight = displayMetrics.heightPixels
+
+        val paddle = findViewById<ImageView>(R.id.paddleImage)
+
+        paddle.setOnTouchListener { _, motionEvent ->
+            when (motionEvent.action) {
+                MotionEvent.ACTION_DOWN -> {
+                    // Handle touch down event
+                    true // Return true to indicate that we have consumed the event
+                }
+                MotionEvent.ACTION_MOVE -> {
+                    // Handle touch move event
+                    val x = motionEvent.rawX
+                    // Move the paddle to the x position of the touch event
+                    paddle.x = x - paddle.width / 2
+                    //paddle.x = motionEvent.x - paddle.left - paddle.width / 2
+                    true // Return true to indicate that we have consumed the event
+                }
+                else -> false // Return false for other touch events
+            }
+        }
+
+
+        val location = IntArray(2)
+
+        //read the top left location
+        paddle.getLocationOnScreen(location)
+
+        //create the edges
+        val x = location[0] + paddle.width / 2
+        val y = location[1] + paddle.height / 2
+
+        fun colision(ball: Any) {
+            //verify if there is a colision with the paddle or the obstacles(rectangles)
+            //which color is the obstacle? (then, change() the color or remove obstacle)
+            //the object of colision is the paddle? (then calculate the speed in order to apply() an effect in the ball)
+
+        }
+
+        val ballImageView = findViewById<ImageView>(R.id.ballImage)
+
+
+        handler = Handler(Looper.getMainLooper())
+
+        // Start the update loop
+        handler.post(object : Runnable {
+            override fun run() {
+                // Update the position of the ball
+                updateBallPosition(ballImageView, screenHeight)
+
+                // Schedule the next update
+                handler.postDelayed(this, updateInterval)
+            }
+        })
+
+
+    }
+
+    //class used to take the edges of any imageView
+    class RectEdges(imageView: ImageView, location: MutableList<Float>) {
+        /*
+
+        var topLeft = mutableListOf<Float>()
+        var topRight = mutableListOf<Float>()
+        var bottomLeft = mutableListOf<Float>()
+        var bottomRight = mutableListOf<Float>()
+
+        init {
+            topLeft = mutableListOf(location[0], location[1])
+            topRight = mutableListOf(location[0] + imageView.width, location[1])
+            bottomLeft = mutableListOf(location[0], location[1] + imageView.height)
+            bottomRight = mutableListOf(location[0] + imageView.width, location[1] + imageView.height)
+        }
+        */
+
+        var top = location[1]
+        var left = location[0]
+        var bottom = location [1] + imageView.height
+        var right = location[0] + imageView.width
+    }
+
+    fun updateBallPosition(imageView: ImageView, screenHeight: Int) {
+
+
+        var imageViewLocation = mutableListOf<Float>(imageView.x, imageView.y)
+        var imageViewEdges = RectEdges(imageView, imageViewLocation)
+
+        if(imageViewEdges.bottom + (standardSpeedY/2 + 1) >= screenHeight) {
+            standardSpeedY = -standardSpeedY
+        }
+
+        // Move the paddle to the x position of the touch event
+        imageView.x = imageView.x + standardSpeedX
+        imageView.y = imageView.y + standardSpeedY
+
+        // Move the ball by changing its position on the screen
+        // You can use the setX() and setY() methods to update the position of the ImageView
+    }
+
+}
